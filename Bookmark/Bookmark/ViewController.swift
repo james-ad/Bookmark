d5a050db665e8dcdd09076c2b318aa016e8d071b
James Dunn <jamesdu@zillowgroup.com>
2021-11-25 08:24:45 -0800

    Capture text from image and populate main label with returned string

--- Bookmark/Bookmark/ViewController.swift
+++ Bookmark/Bookmark/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-//    First, make your view controller conform to both UINavigationControllerDelegate, and UIImagePickerControllerDelegate.
+    //    First, make your view controller conform to both UINavigationControllerDelegate, and UIImagePickerControllerDelegate.
     
     @IBOutlet var captureButton: UIButton!
     @IBOutlet var bookmarkedQuote: UITextView!
@@ -22,21 +23,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     private func confirmPermissionToAccessCamera() {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
-            case .authorized: // The user has previously granted access to the camera.
+        case .authorized: // The user has previously granted access to the camera.
             return
             
-            case .notDetermined: // The user has not yet been asked for camera access.
-                AVCaptureDevice.requestAccess(for: .video) { granted in
-                    if granted {
-                        return
-                    }
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    return
                 }
+            }
             
-            case .denied: // The user has previously denied access.
-                return
-
-            case .restricted: // The user can't grant access due to restrictions.
-                return
+        case .denied: // The user has previously denied access.
+            return
+            
+        case .restricted: // The user can't grant access due to restrictions.
+            return
         @unknown default:
             fatalError("A new camera authorization status has been added without being accounted for in switch statement")
         }
@@ -53,13 +54,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
-
-        guard let image = info[.editedImage] as? UIImage else {
+        
+        guard let image = info[.editedImage] as? UIImage, let cgImage = image.cgImage else {
             print("No image found")
             return
         }
         // print out the image size as a test
         print(image.size)
+        
+        // Create Vision request to read text from image
+        let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, orientation: .up, options: [:])
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let request = VNRecognizeTextRequest(completionHandler: self.recognizeTextHandler)
+            do {
+                try imageRequestHandler.perform([request])
+//                let results = request.results
+            } catch let error as NSError {
+                print("Failed to perform image request: \(error)")
+                return
+            }
+        }
+    }
+    
+    func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let observations =
+                request.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        let recognizedStrings = observations.compactMap { observation in
+            // Return the string of the top VNRecognizedText instance.
+            return observation.topCandidates(1).first?.string
+        }
+        
+        // Process the recognized strings.
+        print("Recognized Strings: \na\(recognizedStrings)")
+        DispatchQueue.main.async {
+            self.bookmarkedQuote.text = recognizedStrings.joined(separator: " ")
+        }
+//        processResults(recognizedStrings)
     }
 }
 
